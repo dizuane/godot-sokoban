fb949a00e8384b345277e5908ca2e590e5ec406b
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-15 15:23:22 -0400

    Added level data, level loading logic

---
+++ singletons/game_data.gd
@@ -0,0 +1,20 @@
+extends Node
+
+const LEVEL_DATA_PATH: String = "res://data/level_data.json"
+const TILE_SIZE: int = 32
+
+var _level_data: Dictionary = {}
+
+
+func _ready():
+	load_level_data()
+
+
+func load_level_data() -> void:
+	var file = FileAccess.open(LEVEL_DATA_PATH, FileAccess.READ)
+	_level_data = JSON.parse_string(file.get_as_text())
+	pass
+
+
+func get_data_for_level(level_num: String) -> Dictionary:
+	return _level_data[level_num]
